163e5bfbde8cab1fda88de40131f702ff987243b
Hippie Hacker <chris@ii.org.nz>
2017-01-20 07:24:17 +1300

    Create .chef symlink

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 mkdir -p /etc/chef
 openssl genrsa -out /etc/chef/client.pem
+cd ../chef-provisioning-k8s
+ln -s ../Chef/.chef .chef
+cd -
 chef-zero -H 0.0.0.0
 #chef-zero -H 0.0.0.0 &
 #berks install && berks upload
